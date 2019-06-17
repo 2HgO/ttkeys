8d0b1ba464677505e4ab067b87f8d04c792ace9f
Tomi Amao <tomi@softcom.ng>
2019-06-17 17:51:47 +0100

    slight updates

--- ttkeys.go
+++ ttkeys.go
@@ -12,6 +12,7 @@ import (
 	"golang.org/x/net/http2"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/aws/endpoints"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/secretsmanager"
@@ -106,28 +107,28 @@ func getSecret(name string, region string) (*secretsmanager.GetSecretValueOutput
 			 To address specific error, you can import this package:
 				"github.com/aws/aws-sdk-go/aws/awserr"
 			and use this example:
-			if aerr, ok := err.(awserr.Error); ok {
-				switch aerr.Code() {
-				case secretsmanager.ErrCodeResourceNotFoundException:
-					fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
-				case secretsmanager.ErrCodeInvalidParameterException:
-					fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
-				case secretsmanager.ErrCodeInvalidRequestException:
-					fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
-				case secretsmanager.ErrCodeDecryptionFailure:
-					fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
-				case secretsmanager.ErrCodeInternalServiceError:
-					fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
-				default:
-					fmt.Println(aerr.Error())
-				}
-			} else {
-				// Print the error, cast err to awserr.Error to get the Code and
-				// Message from an error.
-				fmt.Println(err.Error())
-			}
-			return nil, err
 		*/
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case secretsmanager.ErrCodeResourceNotFoundException:
+				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
+			case secretsmanager.ErrCodeInvalidParameterException:
+				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
+			case secretsmanager.ErrCodeInvalidRequestException:
+				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
+			case secretsmanager.ErrCodeDecryptionFailure:
+				fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
+			case secretsmanager.ErrCodeInternalServiceError:
+				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
+			default:
+				fmt.Println(aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return nil, err
 	}
 
 	return result, nil
